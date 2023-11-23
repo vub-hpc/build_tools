6e17045748b450516c76f9b896e03209c40aa990
sam <sammoors@gmail.com>
2023-11-23 20:25:05 +0100

    fix reaxff software name

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from vsc.install.shared_setup import ad, sm, wp
 
 
 PACKAGE = {
-    'version': '1.1.4',
+    'version': '1.1.5',
     'author': [ad, sm, wp],
     'maintainer': [ad, sm, wp],
     'setup_requires': [
