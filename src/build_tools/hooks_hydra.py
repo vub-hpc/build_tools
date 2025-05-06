252667a84716f0b3813c9b01f50f923503f6c5f5
sam <sammoors@gmail.com>
2025-05-06 10:26:05 +0200

    add zen5

--- src/build_tools/hooks_hydra.py
+++ src/build_tools/hooks_hydra.py
@@ -274,10 +274,11 @@ def parse_hook(ec, *args, **kwargs):  # pylint: disable=unused-argument
 
     # set optarch for intel compilers on AMD nodes
     optarchs_intel = {
-        'zen2': 'march=core-avx2',
+        'zen2': '-march=core-avx2',
         # common-avx512 gives test failure for scipy
         # see https://github.com/easybuilders/easybuild-easyconfigs/pull/18875
-        'zen4': 'march=rocketlake',
+        'zen4': '-march=rocketlake',
+        'zen5': '-march=rocketlake',
     }
     if LOCAL_ARCH in optarchs_intel and ec.toolchain.name in ['intel-compilers', 'iimpi', 'iimkl', 'intel']:
         optarch = ec.toolchain.options.get('optarch')
--- src/build_tools/package.py
+++ src/build_tools/package.py
@@ -16,7 +16,7 @@ Package information of build_tools
 @author: Alex Domingo (Vrije Universiteit Brussel)
 """
 
-VERSION = '4.2.0'
+VERSION = '4.2.1'
 
 AUTHOR = {
     'wp': 'Ward Poelmans',
