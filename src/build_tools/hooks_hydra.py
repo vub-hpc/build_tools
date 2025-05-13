0c3e1f67619ef033dba1673e37d2f0789a66f1bf
Alex Domingo <alex.domingo.toro@vub.be>
2025-05-13 14:44:22 +0200

    enable dynamic loading of FreeSurfer license per-user in bfreesurfer

--- src/build_tools/hooks_hydra.py
+++ src/build_tools/hooks_hydra.py
@@ -484,6 +484,17 @@ end
         if self.name == 'ABAQUS':
             self.cfg['modextravars'].update({'ABAQUSLM_LICENSE_FILE': '/apps/brussel/licenses/abaqus/license.lic'})
 
+        # FreeSurfer per-user licenses
+        if self.name == 'FreeSurfer':
+            self.cfg['modluafooter'] = """
+local username = os.getenv("USER") or ""
+local licfile = pathJoin("/apps/brussel/licenses/freesurfer", username .. ".lic")
+if not isFile(licfile) then
+    LmodError("License file not found for your VSC user. Please consult with user support how to obtain a license for FreeSurfer.")
+end
+setenv("FS_LICENSE", licfile)
+"""
+
         ##########################
         # ------ DATABASES ----- #
         ##########################
